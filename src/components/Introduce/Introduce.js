4c6a4ce79ab3a9d79d7ea9da7ee2224ab59c463e
lenlyinator <tranquanghung24061974@gmail.com>
2024-01-09 20:33:35 +0700

    Change for v0.7.23

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -1,1 +1,11 @@
-export default function Introduce() {}
+import styles from './Introduce.module.scss';
+
+export default function Introduce() {
+  return (
+    <div className={`${styles.container} ${styles['main-content']}`}>
+      <h1 className={styles.title}>
+        <span className={styles['title__h1--text-highlight']}>Who</span> we are?
+      </h1>
+    </div>
+  );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import HeroItems from '../utils/HeroItems/HeroItems';
+import IntroducePart from '../../components/Introduce/Introduce';
 
 export default function Home() {
   return (
     <>
       <HeroItems />
+      <IntroducePart />
     </>
   );
 }
